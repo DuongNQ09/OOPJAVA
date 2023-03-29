249e050eb30423b4a4a1804d6625131acfe069cc
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2023-03-29 17:25:40 +0700

    4

--- GhiDaySo.java
+++ GhiDaySo.java
@@ -1,29 +1,44 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Arrays;
 
 public class GhiDaySo {
     public static void main(String[] args) {
         String fileName = "dayso.dat";
-        int[] numbers = { 10, 20, 30, 40, 50 };
+        int[] numbers = { 10, 50, 30, 40, 20 };
 
-        // Ghi file
         try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+            // Ghi file
             for (int number : numbers) {
                 raf.writeInt(number);
             }
-        } catch (IOException e) {
-            System.out.println("Lỗi ghi file: " + e.getMessage());
-        }
 
-        // Đọc file
-        try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+            // Đọc file
             long fileLength = raf.length();
+            raf.seek(0);
+
+            int[] readNumbers = new int[(int) (fileLength / 4)];
             for (int i = 0; i < fileLength / 4; i++) {
-                int number = raf.readInt();
-                System.out.print(number + " ");
+                readNumbers[i] = raf.readInt();
+            }
+
+            // Sắp xếp và ghi file
+            Arrays.sort(readNumbers);
+            raf.setLength(0);
+
+            for (int number : readNumbers) {
+                raf.writeInt(number);
+            }
+
+            // In dãy số
+            raf.seek(0);
+
+            System.out.print("Dãy số: ");
+            for (int i = 0; i < fileLength / 4; i++) {
+                System.out.print(raf.readInt() + " ");
             }
         } catch (IOException e) {
-            System.out.println("Lỗi đọc file: " + e.getMessage());
+            System.out.println("Lỗi: " + e.getMessage());
         }
     }
 }
