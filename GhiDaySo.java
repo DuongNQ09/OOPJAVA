48b8392cdeee431154e8e9182ac0379f8cb53380
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2023-03-29 17:04:13 +0700

    Create GhiDaySo.java

---
+++ GhiDaySo.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class GhiDocTepNgauNhien {
+    public static void main(String[] args) {
+        String fileName = "dayso.dat";
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        // Ghi file
+        try (RandomAccessFile raf = new RandomAccessFile(fileName, "rw")) {
+            for (int number : numbers) {
+                raf.writeInt(number);
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi ghi file: " + e.getMessage());
+        }
+
+        // Đọc file
+        try (RandomAccessFile raf = new RandomAccessFile(fileName, "r")) {
+            long fileLength = raf.length();
+            for (int i = 0; i < fileLength / 4; i++) {
+                int number = raf.readInt();
+                System.out.print(number + " ");
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file: " + e.getMessage());
+        }
+    }
+}
