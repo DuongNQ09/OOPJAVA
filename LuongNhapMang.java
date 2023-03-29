b0e297ea261df441c729c4d94a1c1fd565b79948
Ngo Quang Duong <96757935+DuongNQ09@users.noreply.github.com>
2023-03-29 16:44:54 +0700

    a

--- LuongNhapMang.java
+++ LuongNhapMang.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
-public class ByteArrayOutput {
+public class LuongNhapMang {
     public static void main(String[] args) {
         byte[] b = new byte[100];
         for (byte i = 0; i < b.length; i++) b[i]=i;
 
         try {
-            OutputStream is = new ByteArrayOutputStream(b);
+            InputStream is = new ByteArrayInputStream(b);
             for(byte i = 0; i < b.length; i++) System.out.print(is.read()+" ");
         }
         catch (IOException e) {
